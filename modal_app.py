628379d3c3379aeef8607407c7c6796f49f32c8b
tqq1994516 <472159011@qq.com>
2026-05-21 15:51:48 +0800

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -13,7 +13,6 @@ image = modal.Image.from_dockerfile(
     secrets=[modal.Secret.from_name("r2-secret")],
     min_containers=0,
     max_containers=1,
-    max_inputs=1000,
     scaledown_window=3600,
 )
 @modal.web_server(8317, startup_timeout=120)
