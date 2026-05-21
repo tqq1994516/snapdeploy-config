b1bab516b30302539797a389778ebf53e28dc302
tqq1994516 <472159011@qq.com>
2026-05-21 13:38:03 +0800

    Initialize modal app with CLI proxy function

---
+++ modal_app.py
@@ -0,0 +1,21 @@
+import modal
+
+app = modal.App()
+
+# 镜像
+image = modal.Image.from_registry(
+    "eceasy/cli-proxy-api:latest",
+    add_python=False
+)
+
+@app.function(
+    image=image,
+    secrets=[modal.Secret.from_name("r2-secret")]
+)
+@modal.web_server(8317, startup_timeout=60)  # 这里定义端口即可！
+def run_cliproxy():
+    import subprocess
+    subprocess.run(
+        ["./CLIProxyAPI"],
+        shell=True,
+    )
